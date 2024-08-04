78a246e53f0e7f9c7379aeb4a92c706266279c34
Mavzz <adityaabhay96@gmail.com>
2024-08-04 11:28:54 +0530

    Readpanda

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.titleContainer}>
+        <Text style={styles.text}>ReadPanda</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#17a589',
+    padding: 50,
+  },
+  titleContainer:{
+    flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+  },
+  text: {
+    color: '#e74c3c',
   },
 });
