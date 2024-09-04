fcc1c6d3bffd9f5b1bbce49a4dfffb3ab0bc1844
Mavzz <adityaabhay96@gmail.com>
2024-09-04 20:58:54 +0530

    abcd

--- App.js
+++ App.js
@@ -1,29 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react'
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 
 export default function App() {
+
+  const [] = useState('');
+  
+  function buttonClicked(){
+    console.log("Button has been clicked");
+    Alert.alert('Button has been clicked');
+  }
+
   return (
     <View style={styles.container}>
-      <View style={styles.titleContainer}>
-        <Text style={styles.text}>ReadPanda</Text>
+      <View style={styles.headerContainer}>
+        <View
+          style={{
+            flex: 1
+          }}>
+        </View>
+        <View style={styles.titleContainer}>
+          <Text style={styles.text}>ReadPanda</Text>
+        </View>
+        <View style={styles.searchButton}>
+          <Button title="Search" color="#FFFF00" onPress={buttonClicked} />
+        </View>
       </View>
-      <StatusBar style="auto" />
+
+      <View>
+        <Text style={{ color: '#e74c3c' }}>Sample texrt</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: '#000000',
     flex: 1,
-    backgroundColor: '#17a589',
-    padding: 50,
+    flexDirection: 'column',
+  },
+  headerContainer:{
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 0,
+    flexDirection: 'row',
+    maxHeight: 90,
   },
   titleContainer:{
-    flex: 1,
+    flex: 3,
     alignItems: 'center',
-    justifyContent: 'flex-start',
+    borderColor: "#ffffff",
+    borderWidth: 1,
+  },
+  searchButton: {
+    flex: 1,
+    
   },
   text: {
     color: '#e74c3c',
+    fontSize: "26px",
   },
 });
