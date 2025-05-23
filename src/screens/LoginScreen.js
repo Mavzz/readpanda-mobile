6f862cf57c91396976ae7aac263c5448eed03461
Venkataramaaditya Nimmagadda <30872381+Mavzz@users.noreply.github.com>
2025-05-24 02:42:42 +0530

    Add Button component with primary and sign-out button functionality
    - Created a new Button.js file in the components directory.
    - Implemented a primary button using Pressable for login actions.
    - Added a SignOutButton component with an icon for user sign-out.
    - Styled buttons using global styles defined in loginStyles.

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,9 +1,17 @@
 import { useState } from "react";
-import { View, Text, TextInput, Alert, ActivityIndicator, Pressable } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  Alert,
+  ActivityIndicator,
+  Pressable,
+} from "react-native";
 import { loginUser, getUserPreferences } from "../services/api";
 import { storeToken } from "../utils/storage";
 import Background from "../components/Background";
-import {loginStyles} from "../styles/global";
+import { loginStyles } from "../styles/global";
+import { primaryButton as PrimaryButton } from "../components/Button";
 
 const Login = ({ navigation }) => {
   const [username, setUsername] = useState("");
@@ -13,7 +21,6 @@ const Login = ({ navigation }) => {
   const handleLogin = async () => {
     setLoading(true);
     try {
-
       const data = await loginUser(username, password);
       console.log(`Root username: ${data.token}`);
       const preferences = await getUserPreferences(data.token, username);
@@ -29,28 +36,34 @@ const Login = ({ navigation }) => {
 
   return (
     <Background>
-      <View style ={loginStyles.container}>
-        <View > 
+      <View style={loginStyles.container}>
+        <View>
           <Text style={loginStyles.mainTitletext}>Login</Text>
-          <View style={{ flexDirection: "row", alignItems: "center",  paddingBottom: 10 }}>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              marginBottom: 15,
+            }}
+          >
             <Text style={loginStyles.title}>Don't have an account?</Text>
             <Pressable onPress={() => navigation.replace("SignUp")}>
               <Text style={loginStyles.signUpText}> sign up!</Text>
             </Pressable>
           </View>
         </View>
-        <View >
+        <View>
           <TextInput
             style={loginStyles.input}
-            placeholder="username"
-            placeholderTextColor="#548C2F"
+            placeholder="Username"
+            placeholderTextColor="#6c757d"
             value={username}
             onChangeText={setUsername}
           />
           <TextInput
             style={loginStyles.input}
             placeholder="Password"
-            placeholderTextColor="#548C2F"
+            placeholderTextColor="#6c757d"
             value={password}
             onChangeText={setPassword}
             secureTextEntry={true}
@@ -59,20 +72,21 @@ const Login = ({ navigation }) => {
         {loading ? (
           <ActivityIndicator size="large" color="#0000ff" />
         ) : (
-          <View>
-            <Pressable onPress={handleLogin} style={loginStyles.loginButton}>
-              <Text style={loginStyles.loginButtonText}>login</Text>
-            </Pressable>
-          </View>
+          <PrimaryButton title="Login" onPress={handleLogin} />
         )}
-        <View>
-          <Text>SSO place holder</Text>
+
+        <View style={loginStyles.dividerContainer}>
+          <View style={loginStyles.divider} />
+          <Text style={loginStyles.orText}>or</Text>
+          <View style={loginStyles.divider} />
         </View>
+
+        <Pressable style={loginStyles.ssoButton}>
+          <Text style={loginStyles.ssoButtonText}>SSO place holder</Text>
+        </Pressable>
       </View>
     </Background>
   );
 };
-
-
 
 export default Login;
